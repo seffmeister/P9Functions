087e60f78d2a3285e6c5778230642329cfa2774e
seffmeister <ismail.sefin@gmail.com>
2024-09-16 18:49:37 +0200

    Implemented movement functions

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//walk three steps forward.
+//then turn around and walk three steps forward
+// then turn left and walk three steps forward.
+// then turn left and walk one step forward.
+// then turn around and walk one step forward
+
+//console.log("Move Forward.");
+//console.log("Move Right");
+
+turnAround();
+stepForward();
+
+function stepForward() {
+  console.log("Move Forward");
+}
+function turnRight() {
+  console.log("Turn Right");
+}
+function turnLeft() {
+  turnRight();
+  turnRight();
+  turnRight();
+}
+function walkThreeSteps() {
+  stepForward();
+  stepForward();
+  stepForward();
+}
+
+function turnAround() {
+  turnRight();
+  turnRight();
+}
